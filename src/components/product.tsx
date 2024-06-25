8124ec0b9eede751c3e06e88b859dab87b57e623
Atharva Pardeshi <atharva.exe@gmail.com>
2024-06-25 17:30:01 +0530

    feat: add edit product button

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -6,6 +6,11 @@ import { Card, CardContent, CardTitle } from "./ui/card"
 
 export default function ProductCard({ product }: { product: Product }) {
   const navigate = useNavigate()
+
+  const handleEdit = () => {
+    navigate(`/products/edit/${product.id}`)
+  }
+
   const handleDelete = () => {
     try {
       actions
@@ -13,7 +18,7 @@ export default function ProductCard({ product }: { product: Product }) {
         .then(res => {
           console.log(res)
         })
-       navigate(0) 
+      navigate(0)
     } catch (error) {
       console.log("Failed to delete product", error)
     }
@@ -31,10 +36,14 @@ export default function ProductCard({ product }: { product: Product }) {
           <CardTitle className="font-semibold tracking-tight">{product.name}</CardTitle>
           <p className="text-4xl font-bold">{product.price}</p>
         </div>
-
-        <Button onClick={handleDelete} variant="destructive" className="absolute bottom-2 right-2">
-          Delete
-        </Button>
+        <div className="flex flex-row gap-2 absolute bottom-2 right-2">
+          <Button onClick={handleEdit} variant="outline" >
+            Edit
+          </Button>
+          <Button onClick={handleDelete} variant="destructive" >
+            Delete
+          </Button>
+        </div>
       </CardContent>
     </Card>
   )
