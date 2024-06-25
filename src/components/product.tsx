14ad02e271455b1b746b833fbb2fc818888565f5
Atharva Pardeshi <atharva.exe@gmail.com>
2024-06-25 16:08:58 +0530

    feat: handle deletion of products

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,8 +1,20 @@
+import actions from "../lib/actions"
 import { Product } from "../types"
 import { Button } from "./ui/button"
-import { Card, CardContent, CardDescription, CardTitle } from "./ui/card"
+import { Card, CardContent, CardTitle } from "./ui/card"
 
 export default function ProductCard({ product }: { product: Product }) {
+  const handleDelete = () => {
+    try {
+      actions
+        .deleteEntry(product.id.toString())
+        .then(res => {
+          console.log(res)
+        })
+    } catch (error) {
+      console.log("Failed to delete product", error)
+    }
+  }
   return (
     <Card className="w-full max-w-sm">
       <CardContent className="relative p-0 overflow-hidden rounded-md">
@@ -12,12 +24,12 @@ export default function ProductCard({ product }: { product: Product }) {
           className="object-cover h-full w-full aspect-[4/3]"
         />
 
-        <CardDescription className="absolute top-4 left-4 bg-opacity-75 bg-white p-2 rounded-md">
+        <div className="absolute top-4 left-4 bg-opacity-75 bg-white p-2 rounded-md">
           <CardTitle className="font-semibold tracking-tight">{product.name}</CardTitle>
-          <div className="text-4xl font-bold">{product.price}</div>
-        </CardDescription>
+          <p className="text-4xl font-bold">{product.price}</p>
+        </div>
 
-        <Button variant="destructive" className="absolute bottom-2 right-2">
+        <Button onClick={handleDelete} variant="destructive" className="absolute bottom-2 right-2">
           Delete
         </Button>
       </CardContent>
