613d15f3f3dacdd37a10e382b47725c88ebbf52f
Atharva Pardeshi <atharva.exe@gmail.com>
2024-06-25 12:57:05 +0530

    feat: render home page, use placeholder products

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,26 @@ import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
 import Layout from "./layout"
 import LoginPage from "./pages/login"
 import React from "react"
+import HomePage from "./pages/home"
+import { Product } from "./types"
+
+let products: Product[] = [
+  {
+    id: 1,
+    name: 'laptop',
+    price: 1000
+  },
+  {
+    id: 2,
+    name: 'phone',
+    price: 500,
+  },
+  {
+    id: 3,
+    name: 'tablet',
+    price: 300
+  }
+]
 
 const ProtectedRoute = ({ children }: { children: React.ReactElement }) => {
   const user = localStorage.getItem("user")
@@ -16,7 +36,13 @@ const ProtectedRoute = ({ children }: { children: React.ReactElement }) => {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <ProtectedRoute><Layout /></ProtectedRoute>
+    element: <ProtectedRoute><Layout /></ProtectedRoute>,
+    children: [
+      {
+        index: true,
+        element: <HomePage products={products} />
+      }
+    ]
   },
   {
     path: "/login",
---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import { Product } from "../types"
+
+const HomePage = ({ products }: { products: Product[] }) => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default HomePage
