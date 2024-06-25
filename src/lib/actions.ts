87fc5b3ffaaedeea91e1f69c9fc8876cf94501be
Atharva Pardeshi <atharva.exe@gmail.com>
2024-06-25 16:07:49 +0530

    feat: crud actions

---
+++ src/lib/actions.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { NewProduct } from "../types";
+const baseUrl = "http://localhost:3000/api/products";
+
+const getAll = async () => {
+  const request = axios.get(baseUrl);
+  return request.then(res => res.data);
+}
+
+const create = async (newObj: NewProduct) => {
+  const request = axios.post(baseUrl, newObj);
+  return request.then(res => res.data);
+}
+
+const deleteEntry = async (id: string) => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(res => res.data);
+}
+
+const update = async (id: string, newObj: NewProduct) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObj);
+  return request.then(res => res.data);
+}
+
+export default { getAll, create, deleteEntry, update }
+
